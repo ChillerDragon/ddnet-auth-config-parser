fcef9f850f212f180e868decda6f750837a4ac5f
ChillerDragon <chillerdragon@gmail.com>
2025-11-02 09:44:40 +0100

    asserts

--- tests/auths.js
+++ tests/auths.js
@@ -1,5 +1,23 @@
-// const assert = require('node:assert').strict
+const assert = require('node:assert').strict
 const path = require('path')
 const authParser = require('../src/auth_parser.js')
 
-authParser.getAuthsSync(path.join(__dirname, '/configs/passwords.cfg'))
+const auths = authParser.getAuthsSync(path.join(__dirname, '/configs/passwords.cfg'))
+assert.deepStrictEqual(auths, [
+  { username: 'foo', role: 'mod', password: 'securepass' },
+  { username: 'bar', role: 'moderator', password: 'quotedpass' },
+  { username: 'baz', role: 'helper', password: 'spaced pass' },
+  { username: 'joe', role: 'helper', password: 'indented entry' },
+  { username: 'spaced name', role: 'mod', password: 'securepass2' },
+  { username: 'mad', role: 'helper', password: 'quotedpass' },
+  {
+    username: 'steve',
+    role: 'helper',
+    password: 'non quoted multi word pass'
+  },
+  {
+    username: 'alice',
+    role: 'helper',
+    password: 'non quoted multi word pass '
+  }
+])
